86f564a12216c6b876b3f6e134b7ff24736a8581
Tomek <nasum2@gmail.com>
2020-07-26 23:23:50 +0200

    build moved to backend

---
+++ backend/build/precache-manifest.9d4721cf0d9eefefac7ccd52949f008c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056985d4033412b3aa7ea66aa0b94e4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84a4f339c382f4a9e0b5",
+    "url": "/static/css/main.7c425bd6.chunk.css"
+  },
+  {
+    "revision": "4582e5743798c9f27203",
+    "url": "/static/js/2.55847924.chunk.js"
+  },
+  {
+    "revision": "54d67a2c3bce32865b370216c2f89a54",
+    "url": "/static/js/2.55847924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a4f339c382f4a9e0b5",
+    "url": "/static/js/main.e1c81566.chunk.js"
+  },
+  {
+    "revision": "3a96a023484963272ffe",
+    "url": "/static/js/runtime-main.24fa6484.js"
+  }
+]);
