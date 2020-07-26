d7c7895235e03ec8aa64ffd230a43805b40b2b17
Tomek <nasum2@gmail.com>
2020-07-26 22:50:32 +0200

    path build removed

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
-const publicPath = path.join(__dirname, '..', "frontend", "build", 'public');
+const publicPath = path.join(__dirname, '..', "frontend", 'public');
 
 app.use(express.static(publicPath));
 
